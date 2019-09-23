99ecf1cabdef8cb5f7f926ec9fbd55c88082aedf
jmotar.29@gmail.com <jmotar.29@gmail.com>
2019-09-23 08:51:32 +0530

    post module

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,6 @@
-angular.module('starter.controllers', ['ionic'])
+angular.module('starter.controllers', ['ionic','firebase'])
 
-.controller('AppCtrl', function($scope,$state, $ionicModal, $timeout) {
+.controller('AppCtrl', function($scope,$state, $ionicModal, $timeout,$ionicPopup,$ionicLoading) {
 
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
@@ -27,20 +27,125 @@ angular.module('starter.controllers', ['ionic'])
     $scope.modal = modal;
   });
 
+  // An alert dialog
+  $scope.showAlert = function(message) {
+    var alertPopup = $ionicPopup.alert({
+      title: 'Error',
+      template: message
+    });
+  };
+
+   // loader
+   $scope.loaderShow = function() {
+    $ionicLoading.show({
+      template: 'Loading...',
+    }).then(function(){
+       console.log("The loading indicator is now displayed");
+    });
+  };
+  $scope.loaderHide = function(){
+    $ionicLoading.hide().then(function(){
+       console.log("The loading indicator is now hidden");
+    });
+  };
+
+  $scope.postPic = "";
+  $scope.uploadPhoto = function(files){
+    $scope.postPic = files[0];
+  }
+
+  $scope.postData = {}
+  $scope.doPost = function(){
+    if(typeof $scope.postData.title=="undefined" || $scope.postData.title==""){
+      $scope.showAlert('Please insert title');
+    }else if(typeof $scope.postData.description=="undefined" || $scope.postData.description==""){
+      $scope.showAlert('Please insert description');
+    }else if($scope.postPic == ""){
+      $scope.showAlert('Please upload post picture');
+    }else{
+      $scope.loaderShow();
+
+      var storageRef = firebase.storage().ref();
+        var uploadTask = storageRef.child('user_post/' + Date.now()).put($scope.postPic);
+
+        uploadTask.on('state_changed', function(snapshot) {
+
+            var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+
+            console.log('Upload is ' + progress + '% done');
+
+            switch (snapshot.state) {
+
+                case firebase.storage.TaskState.PAUSED:
+
+                    console.log('Upload is paused');
+                    break;
+
+                case firebase.storage.TaskState.RUNNING:
+
+                    console.log('Upload is running');
+                    break;
+            }
+
+        }, function(error) {
+
+            alert(error)
+
+        }, function() {
+
+            uploadTask.snapshot.ref.getDownloadURL().then(function(downloadURL) {
+
+              let postData = {
+                title: $scope.postData.title,
+                description: $scope.postData.description,
+                postPic: downloadURL,
+                userId: currentUser.userId,
+                timestamp:Date.now()
+              }
+              firebase.database().ref().child('posts').push(postData);
+
+              $scope.postData = {};
+              $scope.postPic = "";
+              $scope.loaderHide();
+              $scope.modal.hide();
+            });
+        }); 
+
+
+    }
+  }
 })
 
-.controller('PlaylistsCtrl', function($scope) {
-  $scope.playlists = [
-    { title: 'Reggae',writer:'Jitu', id: 1 },
-    { title: 'Chill',writer:'Rahul', id: 2 },
-    { title: 'Dubstep',writer:'Jitu', id: 3 },
-    { title: 'Indie',writer:'Mohan', id: 4 },
-    { title: 'Rap',writer:'Rani', id: 5 },
-    { title: 'Cowbell',writer:'Harsh', id: 6 }
-  ];
+.controller('PlaylistsCtrl', function($scope,$firebaseArray) {
+
+  var ref = firebase.database().ref("posts");
+  $scope.playlists = $firebaseArray(ref);
+  
+  var d = Date(Date.now()); 
+  
+  // Converting the number of millisecond in date string 
+  a = d.toString() 
+  
+  // Printing the current date                     
+  console.log(a);
+
 })
 
-.controller('PlaylistCtrl', function($scope, $stateParams) {
+.controller('PlaylistCtrl', function($scope, $stateParams,$firebaseObject) {
+
+  var post = firebase.database().ref("posts").child($stateParams.playlistId);
+  $scope.postData = $firebaseObject(post);
+
+  $scope.postData.$loaded().then(function(data) {
+    
+    
+    var user = firebase.database().ref("users").child(data.userId);
+    $scope.userData = $firebaseObject(user);
+    // DO WHATEVER
+  }).catch(function(error) {
+    console.error("Error:", error);
+  });
+  
 })
 
 .controller('SigninCtrl', function($scope, $stateParams,$window,$state,$ionicPopup, $timeout,$ionicLoading) {
@@ -149,7 +254,6 @@ angular.module('starter.controllers', ['ionic'])
       firebase.auth().createUserWithEmailAndPassword($scope.signupData.email, $scope.signupData.password)
       .then(function(result) {
 
-        $scope.profilePic
         var storageRef = firebase.storage().ref();
         var uploadTask = storageRef.child('user_profile/' + Date.now()).put($scope.profilePic);
 
